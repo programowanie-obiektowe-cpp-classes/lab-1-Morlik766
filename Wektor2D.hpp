027b99427e746c64032a3ec268082244612d1e3c
Mikołaj Orlik <01171673@pw.edu.pl>
2024-10-21 17:08:07 +0200

    zadanie1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,14 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+private:
+    double x, y;
+public:
+    Wektor2D(): x(0), y(0) {}
+    Wektor2D(double wartoscx, double wartoscy) : x(wartoscx), y(wartoscy) {}
+    double getX() const { return x; }
+    double getY() const { return y; }
+    void setX(double wartoscx) { x = wartoscx; }
+    void setY(double wartoscy) { y = wartoscy; }
+    Wektor2D operator+(const Wektor2D& other) const { return Wektor2D(x + other.x, y + other.y); }
+    double operator*(const Wektor2D& other) const { return (x * other.x + y * other.y); }
 };
